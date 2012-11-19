748dc48c36610ac7abdd15ddd45bf8da75c19c43
Edward Jiang <edj@studentrnd.org>
2012-11-18 18:54:50 -0800

    updated to twiml

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var restler = require('restler');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+	app.all('/', function(request, response) {
+		restler.get('http://reddit.com/.json').on('complete', function(reddit) {
+			var titles = "<Response>";
+			for(var i=0; i<5; i++) {
+				titles += "<Sms>" + reddit.data.children[i].data.title + "</Sms>";
+			}
+			titles += "</Response>";
+			response.send(titles);
+		});
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
